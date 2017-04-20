6f8377c9d2e049ddbdb41707d45d3112c76d636f
stehancocks <ste@twenti3.com>
2017-04-20 21:10:54 +0100

    Version 1

--- Drawing Magic/ViewController.swift
+++ Drawing Magic/ViewController.swift
@@ -10,14 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coloredView: UIView!
+    @IBOutlet weak var newButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupColoredViews()
+        
+        coloredView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+        coloredView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handlePan)))
+    }
+    
+    func setupColoredViews() {
+        let boxDimension = 15
+        let width = self.coloredView.frame.size.width
+        let height = self.coloredView.frame.size.height
+        
+        let numberOfColumns = Int(width) / boxDimension
+        let numberOfRows = Int(height) / boxDimension
+        
+        //   var viewCoordinates = [String:UIView]()
+        
+        for j in 0...numberOfRows {
+            for i in 0...numberOfColumns {
+                let colorView = UIView()
+                colorView.backgroundColor = randomColor()
+                colorView.frame = CGRect(x: i * boxDimension, y: j * boxDimension, width: boxDimension, height: boxDimension)
+                coloredView.addSubview(colorView)
+                //viewCoordinates.["" : colorView]
+            }
+        }
+    }
+    
+    func handlePan(gesture: UIPanGestureRecognizer){
+        let location = gesture.location(in: view)
+        for subview in coloredView.subviews{
+            if subview.frame.contains(location){
+                subview.backgroundColor = .black
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func randomColor() -> UIColor {
+        let red = drand48()
+        let green = drand48()
+        let blue = drand48()
+        return UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 0.7 )
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBAction func newButtonPressed(){
+        for subview in coloredView.subviews{
+            subview.removeFromSuperview()
+        }
+        setupColoredViews()
     }
 
 
